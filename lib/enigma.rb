55394e68f311dbfd9da70fe659f3ae1c7f775414
Chris Smith <42@chrisryansmith.com>
2017-03-27 12:54:37 -0600

    add notes for refactoring ideas

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,16 +5,21 @@ require './lib/offset_calculator'
 class Enigma
 
   def initialize
-    @alpha = ('a'..'z').to_a
-
+    @alpha = ('A'..'Z').to_a
+              + ('a'..'z').to_a
+              + ("0".."9").to_a
+              + %w(! @ # $ % ^ & * ( ) [ ] , . < > ; : / ? |)
+              + [" "]
   end
-
+  # should have crypt class with encrypt and decrypt.
+  # use these as runners
   def encrypt(message, key = nil, date = Date.today)
     key_gen = KeyGenerator.new
     key = key_gen.generate if !key
     offset = OffsetCalculator.new(key, date)
     shift = offset.shift
     letters = message.split('')
+    #move to separate method this will allow using the same method for encrypt/decrypt
     secret = letters.map do |letter|
       index = @alpha.index(letter)
       this_shift = shift[0]
@@ -26,9 +31,39 @@ class Enigma
   end
 
   def decrypt(secret, key, date = Date.today)
+    key_gen = KeyGenerator.new
+    key = key_gen.generate if !key
+    offset = OffsetCalculator.new(key, date)
+    shift = offset.shift
+    # make shifts negative then pass to method from encrypt
+    letters = secret.split('')
+    message = letters.map do |letter|
+      index = @alpha.index(letter)
+      this_shift = shift[0]
+      rotated = @alpha.rotate( -this_shift )
+      shift.rotate!
+      rotated[index]
+    end
+    message.join('')
+  end
+
+  def adjust_end_for_shift(secret)
+    # shift array is four numbers
+    # length of secret % 4 = place in rotation of shift
+    # - move left by the remainder of secret.length % 4 then take the previous four of "..end.."
   end
 
   def crack(secret, date = Date.today)
+    # last 7 characters = "..end.."
+    # recreate the shift array
+    # moving like decrypt (minus)
+    # look at the last char of secret
+    # - find the shift for that to equal "."
+    # - - that gives this_shift+index for one shift value
+    # look at the second to last char of secret
+    # - find the shift for that to equal "."
+    # - - that gives this_shift+index for one shift value
+    # keep repeating until look at all "..end.." chars or filled in the entire shift array
   end
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -18,12 +18,56 @@ class EnigmaTest < MiniTest::Test
   end
 
   def test_encryptor_encrypts_our_names
-    chris = e.encrypt("chris","12345",Date.new(2017,2,23))
-    c_output = "vehkl"
+    chris = e.encrypt("chris ..end..","12345",Date.new(2017,2,23))
+    c_output = "v4BE@Whtx!)tK"
     assert_equal c_output, chris
-    natalia = e.encrypt("natalia","12345",Date.new(2017,2,23))
-    n_output = "gxjcefq"
+    natalia = e.encrypt("natalia ..end..","12345",Date.new(2017,2,23))
+    n_output = "6xD/45&1KO[JwOh"
     assert_equal n_output, natalia
+  end
+
+  def test_encryptor_encrypts_long_with_special_characters
+    first = e.encrypt("a A adslfkjlkjfadsLKJLK: !@ ..end..","12345",Date.new(2017,2,23))
+    c_output = "tWq1t0CHy7>H36]/w^1#ci0xSBV1KO[JwOh"
+    assert_equal c_output, first
+    second = e.encrypt("! @ # $ % ^ & * ( ) [ ] , . < > ; : / ? | ..end..","12345",Date.new(2017,2,23))
+    n_output = "?WV1 WX1BWZ1DWb1FWd1HWf1JWh1LWj1NWl1PWn1RWhtx!)tK"
+    assert_equal n_output, second
+  end
+
+  def test_decryptor_returns_natalia
+    natalia = e.decrypt("6xD/45&1KO[JwOh","12345",Date.new(2017,2,23))
+    n_output = "natalia ..end.."
+    assert_equal n_output, natalia
+  end
+
+  def test_decryptor_returns_chris
+    chris = e.decrypt("v4BE@Whtx!)tK","12345",Date.new(2017,2,23))
+    c_output = "chris ..end.."
+    assert_equal c_output, chris
+  end
+
+  def test_decryptor_decrypts_long_with_special_characters
+    first = e.decrypt("tWq1t0CHy7>H36]/w^1#ci0xSBV1KO[JwOh","12345",Date.new(2017,2,23))
+    c_output = "a A adslfkjlkjfadsLKJLK: !@ ..end.."
+    assert_equal c_output, first
+    second = e.decrypt("?WV1 WX1BWZ1DWb1FWd1HWf1JWh1LWj1NWl1PWn1RWhtx!)tK","12345",Date.new(2017,2,23))
+    n_output = "! @ # $ % ^ & * ( ) [ ] , . < > ; : / ? | ..end.."
+    assert_equal n_output, second
+  end
+
+  def test_adjust_end_for_shift_returns_right_negative_number
+    e.adjust_end_for_shift("")
+  end
+
+  def test_cracker_returns_names
+    skip
+    natalia = e.crack("6xD/45&1KO[JwOh",Date.new(2017,2,23))
+    n_output = "natalia"
+    assert_equal n_output, natalia
+    chris = e.decrypt("v4BE@Whtx!)tK",Date.new(2017,2,23))
+    c_output = "chris"
+    assert_equal c_output, chris
   end
 
 end
