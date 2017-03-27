0a569d76fa1a0579e3355a52d572c25b623c5a78
Chris Smith <42@chrisryansmith.com>
2017-03-27 14:22:10 -0600

    fix single quote fix

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -18,13 +18,13 @@ class EnigmaTest < MiniTest::Test
     c_output = "v4AD@Rfrx![rI"
     assert_equal c_output, chris
     natalia = e.encrypt("natalia ..end..","12345",Date.new(2017,2,23))
-    n_output = '6xC\"45*wIM]IwMf'
+    n_output = "6xC\"45*wIM]IwMf"
     assert_equal n_output, natalia
   end
 
   def test_encryptor_encrypts_long_with_special_characters
     first = e.encrypt("a A adslfkjlkjfadsLKJLK: !@ ..end..","12345",Date.new(2017,2,23))
-    c_output = 'tRqwt0BGy7;G36,\"w&1$ci0vNAUwIM]IwMf'
+    c_output = "tRqwt0BGy7;G36,\"w&1$ci0vNAUwIM]IwMf"
     assert_equal c_output, first
     second = e.encrypt("! @ # $ % ^ & * ( ) [ ] , . < > ; : / ? | ..end..","12345",Date.new(2017,2,23))
     n_output = "/RUwSRVw#RXwBRZwDRbwFRdwHRfwJRhwLRjwPRnwRRfrx![rI"
@@ -32,7 +32,7 @@ class EnigmaTest < MiniTest::Test
   end
 
   def test_decryptor_returns_natalia
-    natalia = e.decrypt('6xC\"45*wIM]IwMf',"12345",Date.new(2017,2,23))
+    natalia = e.decrypt("6xC\"45*wIM]IwMf","12345",Date.new(2017,2,23))
     n_output = "natalia ..end.."
     assert_equal n_output, natalia
   end
@@ -44,7 +44,7 @@ class EnigmaTest < MiniTest::Test
   end
 
   def test_decryptor_decrypts_long_with_special_characters
-    first = e.decrypt('tRqwt0BGy7;G36,\"w&1$ci0vNAUwIM]IwMf',"12345",Date.new(2017,2,23))
+    first = e.decrypt("tRqwt0BGy7;G36,\"w&1$ci0vNAUwIM]IwMf","12345",Date.new(2017,2,23))
     c_output = "a A adslfkjlkjfadsLKJLK: !@ ..end.."
     assert_equal c_output, first
     second = e.decrypt("/RUwSRVw#RXwBRZwDRbwFRdwHRfwJRhwLRjwPRnwRRfrx![rI","12345",Date.new(2017,2,23))
@@ -58,7 +58,7 @@ class EnigmaTest < MiniTest::Test
 
   def test_cracker_returns_names
     skip
-    natalia = e.crack('6xC\"45*wIM]IwMf',Date.new(2017,2,23))
+    natalia = e.crack("6xC\"45*wIM]IwMf",Date.new(2017,2,23))
     n_output = "natalia"
     assert_equal n_output, natalia
     chris = e.decrypt("v4AD@Rfrx![rI",Date.new(2017,2,23))
